ee818f87ee2af4e0e047e01b621064c01958c504
Boofny <Brandonpizano7557@gmail.com>
2025-09-28 15:21:08 -0400

    feat commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,12 @@
+
 document.getElementById("But").addEventListener("click", async () => {
 	try{
 		const resp = await fetch("http://localhost:8081/test")
 		const data = await resp.json()
   	document.getElementById("display").textContent = data.hello
+    console.log("Good")
 	}catch(err){
   	document.getElementById("display").textContent = "Error"
+    console.log("Error")
   }
 })
